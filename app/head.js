7702d4976afd4210f4a5717ab97c62a660526e2f
ToniIAPro73 <supertoniia@gmail.com>
2025-11-02 00:00:52 +0100

    nuevos cambios

---
+++ app/head.js
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="cache-control"
+        content="no-cache, no-store, must-revalidate"
+      />
+      <meta httpEquiv="expires" content="0" />
+      <meta httpEquiv="pragma" content="no-cache" />
+      <meta name="robots" content="noindex, nofollow" />
+      <title>
+        Playa Viva - Inversión Inmobiliaria en Al Marjan Island | Uniestate
+      </title>
+    </>
+  );
+}
